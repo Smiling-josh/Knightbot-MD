9f25037a0874b2999f84144ed644df7a36474029
Smiling-josh <asaborojoshuaovuoke@gmail.com>
2025-05-26 06:40:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'SmilingJosh Bot',
+  author: '‎Bot',
+  botName: "SmilingJosh Bot",
+  botOwner: 'SmilingJosh', // Your name
+  ownerNumber: '2348102994496', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
